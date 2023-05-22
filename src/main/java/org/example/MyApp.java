716776ac28fa1170504890078345cc1798c27383
MohitVishwakar <mohitviswakrama78@gmail.com>
2023-05-22 22:01:04 +0530

    My first commit

---
+++ src/main/java/org/example/MyApp.java
@@ -0,0 +1,24 @@
+package org.example;//package com.mycompany.brick;
+
+//import org.example;
+import org.example.GamePlay;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author chinm
+ */
+public class MyApp {
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        GamePlay gameplay = new GamePlay();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("BrickBreaker");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gameplay);
+    }
+
+}
